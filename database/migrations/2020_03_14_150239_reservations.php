f64c4e70b51afbb73da087ab3168c9c13aad3703
unknown <ralphlaynes@gmail.com>
2020-03-14 23:16:18 +0800

    Reserfvation Controller latest

---
+++ database/migrations/2020_03_14_150239_reservations.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Reservations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('reservation_id');
+            $table->integer('room_id');
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('txn_id');
+            $table->integer('verification_number');
+            $table->timestamp('created_at');
+            $table->string('created_by')->nullable();
+            $table->integer('tag_deleted')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
