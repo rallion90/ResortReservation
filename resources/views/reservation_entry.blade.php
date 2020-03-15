b5d909ee9760c29439e57adf0ea372ac9554f6a2
unknown <ralphlaynes@gmail.com>
2020-03-15 16:46:40 +0800

    Changes

---
+++ resources/views/reservation_entry.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+@section('content')
+<form action="">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+
+				<div class="form-header">
+					<h2>Make your reservation</h2>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<span class="form-label">Check In</span>
+							<input class="form-control" type="date" required>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="form-group">
+							<span class="form-label">Check Out</span>
+							<input class="form-control" type="date" required>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="form-group">
+							<span class="form-label">Room Name</span>
+							<input class="form-control" type="text" disabled value="Room Name" required>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="form-group">
+							<span class="form-label">Room Price</span>
+							<input class="form-control" disabled value="1000" type="text" required>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+
+			<div class="col-md-6">
+				<div class="for-header">
+					<h2>Customer Information</h2>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<span class="form-label">First Name</span>
+							<input class="form-control" type="text" required>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="form-group">
+							<span class="form-label">Last Name</span>
+							<input class="form-control" type="text" required>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="form-group">
+							<span class="form-label">Email</span>
+							<input class="form-control" type="text" required>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="form-group">
+							<button class="btn btn-info">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+@endsection
